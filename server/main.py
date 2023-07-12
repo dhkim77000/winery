d54aac42e8d943837af67222fbb15665fd15cbf1
PoPoMonS <s25n.gh5@gamil.com>
2023-07-12 02:37:10 +0000

    Add CORS Middleware

--- server/main.py
+++ server/main.py
@@ -3,8 +3,20 @@ from fastapi import FastAPI, Form, Request
 
 import uvicorn
 from routers import user_router, home_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router.router)
 app.include_router(home_router.router)
 
