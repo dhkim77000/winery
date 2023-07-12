7411c3720019898e630d3338fe5934bb547fb46d
dhkim77000 <dhkim77000@gmail.com>
2023-07-12 21:05:05 +0900

    modify project dir

--- code/text/train_bert_multilabel.py
+++ code/text/train_bert_multilabel.py
@@ -24,6 +24,7 @@ import logging
 from torch.utils.data.dataset import Dataset
 from transformers import DataCollatorForLanguageModeling
 import os
+from sklearn import metrics
 import gc
 from transformers import Trainer, TrainingArguments, AutoTokenizer
 from transformers.utils import logging
@@ -35,59 +36,16 @@ from dataset import TextDatasetForNextSentencePrediction
 from transformers import BertConfig, BertForPreTraining
 from tokenizers import BertWordPieceTokenizer
 import argparse
+from sklearn.metrics import log_loss, accuracy_score, precision_score, recall_score
 from model import BERTClass
 from dataset import MultilabelDataset
-
+from train_utils import train, validation
 # Creating the customized model, by adding a drop out and a dense layer on top of distil bert to get the final output for the model. 
 
-def loss_fn(outputs, targets):
-    return torch.nn.BCEWithLogitsLoss()(outputs, targets)
-
-def train(args, model, optimizer, training_loader):
-    model.train()
-    for data in tqdm(training_loader):
-        ids = data['ids'].to(args.device, dtype = torch.long)
-        mask = data['mask'].to(args.device, dtype = torch.long)
-        token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
-        targets = data['targets'].to(args.device, dtype = torch.float)
-
-        outputs = model(ids, mask, token_type_ids)
-
-        optimizer.zero_grad()
-        loss = loss_fn(outputs, targets)
-        if _%5000==0:
-            print(f'Epoch: {epoch}, Loss:  {loss.item()}')
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        del token_type_ids, ids, mask, outputs, loss, data
-        gc.collect()
-    return model
-
-def validation(args, model, epoch, testing_loader):
-    model.eval()
-    fin_targets=[]
-    fin_outputs=[]
-    with torch.no_grad():
-        for _, data in enumerate(testing_loader):
-            ids = data['ids'].to(args.device, dtype = torch.long)
-            mask = data['mask'].to(args.device, dtype = torch.long)
-            token_type_ids = data['token_type_ids'].to(args.device, dtype = torch.long)
-            targets = data['targets'].to(args.device, dtype = torch.float)
-            outputs = model(ids, mask, token_type_ids)
-            fin_targets.extend(targets.cpu().detach().numpy().tolist())
-            fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
-
-            del targets, ids, mask, outputs, data
-            gc.collect()
-
-    return fin_outputs, fin_targets
 
 def run(args, model, optimizer,training_loader,testing_loader):
     for epoch in range(args.epochs):
-        model = train(args, model, optimizer, training_loader)
+        model = train(args, model, optimizer, training_loader, epoch)
         outputs, targets = validation(args, model, epoch,testing_loader)
 
         outputs = np.array(outputs) >= 0.5
