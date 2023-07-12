9bfc527e63f1c0eb8f092b95efadaac25ccb35a5
dhkim77000 <dhkim77000@gmail.com>
2023-07-12 16:08:27 +0900

    bert review embedding

--- code/data/merge_data.py
+++ code/data/merge_data.py
@@ -21,13 +21,19 @@ def merge(args):
     for i in tqdm(range(6)):
 
         if i == 0:
-            data = pd.read_csv(f'/opt/ml/wine/data/{args.dataset_name}0.csv', encoding = 'utf-8-sig')
+            data = pd.read_csv(f'/opt/ml/wine/data/{args.dataset_name}_total.csv', encoding = 'utf-8-sig')
+            data.dropna(inplace=True)
         else:
             tmp = pd.read_csv(f'/opt/ml/wine/data/{args.dataset_name}{i}.csv', encoding = 'utf-8-sig')
+            tmp.dropna(inplace=True)
             data = pd.concat([data, tmp], axis=0)
+
+    tmp = pd.read_csv(f'/opt/ml/wine/data/{args.dataset_name}.csv', encoding = 'utf-8-sig')
+    data = pd.concat([data, tmp], axis=0)
 
     bf = len(data)
     data.drop_duplicates(inplace = True)
+    
     af = len(data)
     data.to_csv(f'/opt/ml/wine/data/review_df_total.csv', encoding = 'utf-8-sig', index= False)
     print(f"Before drop duplicates {bf}, After {af}")
