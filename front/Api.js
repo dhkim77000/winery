ba81a4f4d23e05438268ddd21c3320ba98a0bad9
PoPoMonS <s25n.gh5@gamil.com>
2023-07-13 08:44:05 +0000

    Add api

---
+++ front/Api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+	baseURL: "http://34.125.168.27:8000/", // FastAPI 백엔드의 기본 URL을 여기에 입력합니다.
+});
+
+export const getApi = (endpoint) => {
+    // endpoint: login/, login/register/, home/ ...
+	return api.get(endpoint); // 백엔드에서 제공하는 엔드포인트로 GET 요청을 보냅니다.
+};
+
+export const postApi = (endpoint, data) => {
+    // endpoint: login/, login/register/, home/ ...
+	return api.post(endpoint, data); // 엔드포인트로 POST 요청을 보냅니다.
+};
