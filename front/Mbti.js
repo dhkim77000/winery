b9db72debef166bade721e1cd993e471efb68b31
yewonhihi <yewon1118@naver.com>
2023-07-28 11:09:07 +0900

    [Feat][yewon] add MBTI counting & winestyle
    Signed-off-by: yewonhihi <yewon1118@naver.com>

--- front/Mbti.js
+++ front/Mbti.js
@@ -1,176 +1,394 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+  Image,
+  ScrollView,
+} from "react-native";
 import questions from "./MbtiQ";
+import mbtiStyle from "./MbtiS";
 import { useForm } from "react-hook-form";
 import { postApi } from "./Api";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Mbti({ navigation: { navigate, goBack }, route }) {
-	console.log(route.params.email);
-	console.log(route.params.password);
-	const { register, setValue, watch } = useForm();
-	const [questionNum, setQuestionNum] = useState(0); // 문항번호 0~9
-	const [result, setResult] = useState([]); // 고른 결과 리스트
+  //console.log(route.params.email);
+  //console.log(route.params.password);
+  const { register, setValue, watch } = useForm();
+  const [questionNum, setQuestionNum] = useState(0); // 문항번호 0~9
+  const [result, setResult] = useState([]); // 고른 결과 리스트
+  const [count, setCount] = useState(Array.from({ length: 23 }, () => 0));
+  const [select, setSelect] = useState(""); // 사진 성별 선택
+  const [ran, setRan] = useState(0);
+  const [postResult, setPostResult] = useState([]); // 보내줄 결과 리스트
+  const [finalId, setFinalId] = useState(null);
 
-	useEffect(() => {
-		register("email", {
-			required: true,
-		});
-		register("password", {
-			required: true,
-		});
-		register("mbti_result", {
-			required: true,
-		});
-		setValue("email", route.params.email);
-		setValue("password", route.params.password);
-	}, []);
+  useEffect(() => {
+    register("email", {
+      required: true,
+    });
+    register("password", {
+      required: true,
+    });
+    register("mbti_result", {
+      required: true,
+    });
+    register("wine_style", {
+      required: true,
+    });
+    setValue("email", route.params.email);
+    setValue("password", route.params.password);
+    setRan(Math.random()); // 랜덤 상수
+  }, []);
 
-	const isPress = (item) => {
-		// 선택지 눌렀을 때
-		setResult([...result, item.label]);
-		setQuestionNum(questionNum + 1);
-	};
+  const isPress = (item) => {
+    // 선택지 눌렀을 때
+    item.styles.map((id) => {
+      count[id]++;
+      setCount([...count]);
+    });
+    if (questionNum < 5) {
+      setPostResult([...postResult, item.postLabel]);
+    }
+    if (questionNum === 9) {
+      setValue("mbti_result", postResult);
+      const indexResult = []; // maxValue의 인덱스값이 여러개일 상황고려하여 배열에 저장
+      const maxValue = Math.max(...count);
+      let maxIndex = count.indexOf(maxValue);
+      while (maxIndex != -1) {
+        indexResult.push(maxIndex);
+        maxIndex = count.indexOf(maxValue, maxIndex + 1);
+      }
+      setFinalId(indexResult[Math.floor(ran * indexResult.length)]);
+    }
+    setResult([...result, item.label]);
+    setQuestionNum(questionNum + 1);
+  };
 
-	const onValid = async (data) => {
-		const endpoint = "login/register/";
-		console.log(data);
-		try {
-			const response = await postApi(endpoint, data);
-			console.log(response.data);
-		} catch (error) {
-			alert(error);
-			console.log(error);
-		}
-	};
+  const onValid = async (data) => {
+    const endpoint = "login/register/";
+    console.log(data);
+    try {
+      const response = await postApi(endpoint, data);
+      //console.log(response.data);
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
 
-	const isBack = () => {
-		// back 눌렀을 때 전 문제로 돌아가기
-		setQuestionNum(questionNum - 1);
-		result.pop();
-		setResult([...result]);
-	};
+  const isBack = () => {
+    // back 눌렀을 때 전 문제로 돌아가기
+    if (questionNum != 0) {
+      postResult.pop();
+      setPostResult([...postResult]);
+      const back = result.pop();
+      if (questions[questionNum - 1].answers[back - 1].styles) {
+        questions[questionNum - 1].answers[back - 1].styles.map((id) => {
+          count[id]--;
+          setCount([...count]);
+        });
+      } else {
+        console.warn("Styles not found for the selected answer.");
+      }
+      setResult([...result]);
+      setQuestionNum(questionNum - 1);
+    } else {
+      goBack();
+    }
+  };
 
-	console.log(result); // 결과 잘 나오는지 실험용
+  //console.log(result); // 결과 잘 나오는지 실험용
+  //console.log(postResult);
 
-	if (questionNum === 10) {
-		//완료 페이지
-		return (
-			<View style={styles.wrapper}>
-				<Text style={{ fontSize: 25, marginBottom: 20 }}>결과</Text>
-				<Text>{result}</Text>
-				<TouchableOpacity
-					style={styles.doneBtn}
-					onPress={() => {
-						setValue("mbti_result", result);
-						onValid(watch());
-						navigate("Login");
-					}}
-				>
-					<Text>처음으로 돌아가기</Text>
-				</TouchableOpacity>
-			</View>
-		);
-	} else if ((questionNum < 10) & (questionNum > -1)) {
-		return (
-			<View style={styles.wrapper}>
-				<View style={styles.progressBarOut}>
-					<View
-						style={barStyle(`${(questionNum + 1) * 10}%`).progressBarIn}
-					></View>
-				</View>
-				<Text style={styles.title}>{questions[questionNum].question}</Text>
-				<View style={styles.subContainer}>
-					{questions[questionNum].answers.map((item) => {
-						return (
-							<TouchableOpacity
-								key={item.label}
-								onPress={() => isPress(item)}
-								style={{
-									width: "80%",
-									padding: 20,
-									margin: 15,
-									borderColor: "#FFC0CB",
-									borderWidth: 2,
-									borderRadius: 20,
-									// alignItems: "center", // text 가운데 정렬
-								}}
-								activeOpacity={0}
-							>
-								<Text>{item.text}</Text>
-							</TouchableOpacity>
-						);
-					})}
-				</View>
-				<View style={styles.backContainer}>
-					<TouchableOpacity style={styles.backBtn} onPress={() => isBack()}>
-						<Text style={{ fontSize: 20 }}>back</Text>
-					</TouchableOpacity>
-				</View>
-			</View>
-		);
-	} else {
-		// 전 sign 페이지로 back
-		goBack();
-	}
+  if (questionNum === 11) {
+    //완료 페이지
+
+    return (
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          style={{
+            backgroundColor: "#FFC0CB",
+          }}
+          contentContainerStyle={{
+            alignItems: "center",
+            //justifyContent: "center",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              margin: 10,
+              marginTop: 30,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                setSelect(mbtiStyle[finalId].mPic);
+              }}
+              style={{ marginRight: 20, alignItems: "center" }}
+            >
+              <Text style={{ fontSize: 10 }}>man</Text>
+              <Icon name={"face-man"} size={50} color={"#000000"} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setSelect(mbtiStyle[finalId].wPic);
+              }}
+              style={{ marginLeft: 20, alignItems: "center" }}
+            >
+              <Text style={{ fontSize: 10 }}>woman</Text>
+              <Icon name={"face-woman"} size={50} color={"#000000"} />
+            </TouchableOpacity>
+          </View>
+          <Image
+            style={{
+              marginTop: 10,
+              marginBottom: 20,
+              ...Platform.select({
+                ios: { width: 300, height: 300 },
+                android: { width: 300, height: 300 },
+                web: { width: 450, height: 450 },
+              }),
+            }}
+            source={select === "" ? setSelect(mbtiStyle[finalId].mPic) : select}
+          />
+          <Text>당신을 닮은 와인스타일은</Text>
+          <Text
+            style={{
+              fontSize: 30,
+              margin: 20,
+              textAlign: "center",
+            }}
+          >
+            {mbtiStyle[finalId].mStyle}
+          </Text>
+          <Text style={{ width: "75%", fontSize: 15, lineHeight: 28 }}>
+            {mbtiStyle[finalId].text}
+          </Text>
+          <TouchableOpacity
+            style={styles.doneBtn}
+            onPress={() => {
+              navigate("Login");
+            }}
+          >
+            <Text>처음으로 돌아가기</Text>
+          </TouchableOpacity>
+          <View style={{ height: 80 }} />
+        </ScrollView>
+      </View>
+    );
+  } else if (questionNum === 10) {
+    return (
+      <View
+        style={[
+          styles.wrapper,
+          { justifyContent: "center", alignItems: "center" },
+        ]}
+      >
+        <TouchableOpacity style={styles.lastBtn} onPress={() => isBack()}>
+          <Text style={{ fontSize: 20 }}>back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.lastBtn}
+          onPress={() => {
+            if (finalId) {
+              setValue("wine_style", mbtiStyle[finalId].mStyle);
+              onValid(watch());
+              setQuestionNum(questionNum + 1);
+            }
+          }}
+        >
+          <Text style={{ fontSize: 20 }}>결과</Text>
+          <Text style={{ fontSize: 20 }}>보러가기</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  } else if (questionNum === 1) {
+    return (
+      <View style={styles.wrapper}>
+        <View style={styles.progressBarOut}>
+          <View
+            style={barStyle(`${(questionNum + 1) * 10}%`).progressBarIn}
+          ></View>
+        </View>
+        <Text style={styles.title}>{questions[questionNum].question}</Text>
+        <View
+          style={[
+            styles.subContainer,
+            {
+              flexDirection: "row",
+              justifyContent: "center",
+              textAlign: "center",
+              paddingBottom: 20,
+            },
+          ]}
+        >
+          {questions[questionNum].answers &&
+            questions[questionNum].answers.map(
+              // map undefined 오류 방지
+              (item) => {
+                return (
+                  <View key={item.label}>
+                    <TouchableOpacity
+                      onPress={() => isPress(item)}
+                      style={{
+                        ...Platform.select({
+                          web: {
+                            width: 60,
+                            height: 60,
+                            borderRadius: 30,
+                            margin: 18,
+                          },
+                          ios: {
+                            width: 40,
+                            height: 40,
+                            borderRadius: 20,
+                            margin: 15,
+                          },
+                          android: {
+                            width: 55,
+                            height: 55,
+                            borderRadius: 27.5,
+                            margin: 10,
+                          },
+                        }),
+                        borderColor: "#FFC0CB",
+                        borderWidth: 2,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginBottom: 30,
+                        marginTop: 30,
+                      }}
+                      activeOpacity={0}
+                    ></TouchableOpacity>
+                    <Text style={{ textAlign: "center" }}>{item.text}</Text>
+                  </View>
+                );
+              }
+            )}
+        </View>
+        <View style={styles.backContainer}>
+          <TouchableOpacity style={styles.backBtn} onPress={() => isBack()}>
+            <Text style={{ fontSize: 20 }}>back</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  } else if ((questionNum < 10) & (questionNum > -1)) {
+    return (
+      <View style={styles.wrapper}>
+        <View style={{ height: 50 }} />
+        <View style={styles.progressBarOut}>
+          <View
+            style={barStyle(`${(questionNum + 1) * 10}%`).progressBarIn}
+          ></View>
+        </View>
+        <Text style={styles.title}>{questions[questionNum].question}</Text>
+        <View style={styles.subContainer}>
+          {questions[questionNum].answers.map((item) => {
+            return (
+              <TouchableOpacity
+                key={item.label}
+                onPress={() => isPress(item)}
+                style={{
+                  width: "80%",
+                  padding: 20,
+                  margin: 15,
+                  borderColor: "#FFC0CB",
+                  borderWidth: 2,
+                  borderRadius: 20,
+                  // alignItems: "center", // text 가운데 정렬
+                }}
+                activeOpacity={0}
+              >
+                <Text>{item.text}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+        <View style={styles.backContainer}>
+          <TouchableOpacity style={styles.backBtn} onPress={() => isBack()}>
+            <Text style={{ fontSize: 20 }}>back</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const barStyle = (completed) =>
-	StyleSheet.create({
-		progressBarIn: {
-			height: "100%",
-			backgroundColor: "#F48FB1",
-			borderRadius: 100,
-			width: completed,
-		},
-	});
+  StyleSheet.create({
+    progressBarIn: {
+      height: "100%",
+      backgroundColor: "#F48FB1",
+      borderRadius: 100,
+      width: completed,
+    },
+  });
 
 const styles = StyleSheet.create({
-	wrapper: {
-		flex: 1,
-		backgroundColor: "#FFC0CB",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	backContainer: {
-		alignItems: "flex-start",
-		justifyContent: "center",
-		width: "60%",
-	},
-	title: {
-		textAlign: "center",
-		marginBottom: 10,
-		marginTop: 30,
-		fontSize: 20,
-	},
-	subContainer: {
-		backgroundColor: "#FFFFFF",
-		borderRadius: 20,
-		width: "80%",
-		margin: 20,
-		alignItems: "center",
-		padding: 10,
-	},
-	backBtn: {
-		width: "30%",
-		borderRadius: 25,
-		height: 50,
-		alignItems: "center",
-		justifyContent: "center",
-		backgroundColor: "#F48FB1",
-	},
-	doneBtn: {
-		width: "80%",
-		borderRadius: 25,
-		height: 50,
-		alignItems: "center",
-		justifyContent: "center",
-		marginTop: 40,
-		backgroundColor: "#F48FB1",
-	},
-	progressBarOut: {
-		height: 20,
-		width: "80%",
-		backgroundColor: "#FFFFFF",
-		borderRadius: 100,
-	},
+  wrapper: {
+    flex: 1,
+    backgroundColor: "#FFC0CB",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backContainer: {
+    alignItems: "flex-start",
+    justifyContent: "center",
+    width: "70%",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 10,
+    marginTop: 30,
+    fontSize: 20,
+  },
+  subContainer: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 20,
+    width: "80%",
+    margin: 20,
+    alignItems: "center",
+  },
+  backBtn: {
+    width: 80,
+    borderRadius: 40,
+    height: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F48FB1",
+    marginTop: 30,
+  },
+  lastBtn: {
+    width: "25%",
+    borderRadius: 30,
+    height: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F48FB1",
+    margin: 30,
+    padding: 10,
+  },
+  doneBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#F48FB1",
+  },
+  progressBarOut: {
+    height: 20,
+    width: "80%",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 100,
+    //marginTop: 50,
+  },
 });
--- front/MbtiQ.js
+++ front/MbtiQ.js
@@ -1,104 +1,240 @@
 const questions = [
-    {
-        unique_id: 0,
-        question: '선호하는 선물 스타일은?',
-        answers: [
-            {text: '감동적인 선물', label: 1},
-            {text: '유용한 선물', label: 2},
-            {text: '독특한 선물', label: 3},
-            {text: '세련된 선물', label: 4},
-        ]
-    },
-    {
-        unique_id: 1,
-        question: '아이템을 고를 때 중요시하는 기준은?',
-        answers: [
-            {text: '품질과 성능', label: 1},
-            {text: '디자인', label: 2},
-            {text: '가격', label: 3},
-            {text: '인지도', label: 4},
-        ]
-    },
-    {
-        unique_id: 2,
-        question: '주로 어떤 유형의 사람들과 어울리는 것을 선호하나요?',
-        answers: [
-            {text: '활기찬 사교적인 사람들', label: 1},
-            {text: '차분한 사람들', label: 2},
-            {text: '예술가나 엔터테이너', label: 3},
-            {text: '독특한 사람들', label: 4},
-        ]
-    },
-    {
-        unique_id: 3,
-        question: '좋아하는 옷 스타일은?',
-        answers: [
-            {text: '캐쥬얼한 스타일', label: 1},
-            {text: '미니멀한 스타일', label: 2},
-            {text: '빈티지한 스타일', label: 3},
-            {text: '트렌디하고 힙한 스타일', label: 4},
-        ]
-    },
-    {
-        unique_id: 4,
-        question: '휴일이나 주말, 나의 취향은?',
-        answers: [
-            {text: '책이나 음악 감상', label: 1},
-            {text: '친구들과 액티비티', label: 2},
-            {text: '맛집 탐방', label: 3},
-            {text: '등산이나 운동', label: 4},
-        ]
-    },
-    {
-        unique_id: 5,
-        question: '식사 때, 어떤 음식과 함께 와인을 즐기고 싶으신가요?',
-        answers: [
-            {text: '해산물 플레터', label: 1},
-            {text: '파스타와 피자', label: 2},
-            {text: '치즈 및 디저트', label: 3},
-            {text: '미트 플레터', label: 4},
-        ]
-    },
-    {
-        unique_id: 6,
-        question: '당신의 성격은 어떻게 표현하시겠어요?',
-        answers: [
-            {text: '유쾌, 친근함', label: 1},
-            {text: '진중하고 자신감 넘침', label: 2},
-            {text: '밝고 활기참', label: 3},
-            {text: '차분하고 우아함', label: 4},
-        ]
-    },
-    {
-        unique_id: 7,
-        question: '당신이 좋아하는 향기는 무엇인가요?',
-        answers: [
-            {text: '베리류 향기', label: 1},
-            {text: '시트러스, 청량한 과일 향기', label: 2},
-            {text: '열대 과일 향기', label: 3},
-            {text: '빵, 버터 향기', label: 4},
-        ]
-    },
-    {
-        unique_id: 8,
-        question: '주로 어떤 종류의 영화를 선호하시나요?',
-        answers: [
-            {text: '멜로/로맨스 영화', label: 1},
-            {text: '액션/모험 영화', label: 2},
-            {text: '드라마/스릴러 영화', label: 3},
-            {text: '코미디/로맨틱 코미디', label: 4},
-        ]
-    },
-    {
-        unique_id: 9,
-        question: '휴가를 가기 위해 꿈꾸는 목적지는 어디인가요?',
-        answers: [
-            {text: '해변 휴가', label: 1},
-            {text: '자연과 풍경 감상', label: 2},
-            {text: '역사적인 도시 탐방', label: 3},
-            {text: '놀이공원과 테마파크', label: 4},
-        ]
-    },
-]
+  {
+    unique_id: 0,
+    question: "어떤 스타일의 와인을 가장 드시고 싶나요?",
+    answers: [
+      {
+        text: "다양한 베리류, 오크, 가죽 향과 복합적인 풍미가 느껴지는 맛",
+        postLabel: "a1",
+        label: 1,
+        styles: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+      },
+      {
+        text: "열대 과일, 시트러스 향과 청량감과 상큼함이\n느껴지는 맛",
+        postLabel: "a2",
+        label: 2,
+        styles: [4, 5, 6, 7, 8, 9],
+      },
+      {
+        text: "말린 과일, 탄산감과 청량함이 느껴지는 맛",
+        postLabel: "a3",
+        label: 3,
+        styles: [0, 1, 2, 3],
+      },
+    ],
+  },
+  {
+    unique_id: 1,
+    question: "와인을 마시고 원하는 입 안의 상태는 ?",
+    answers: [
+      {
+        text: "풍부하고\n깊은\n입안 감촉",
+        postLabel: "b1",
+        label: 1,
+        styles: [0, 4, 11, 21],
+      },
+      {
+        text: "\n\n",
+        postLabel: "b2",
+        label: 2,
+        styles: [1, 2, 6, 10, 12, 13, 14, 15, 18, 20, 22],
+      },
+      { text: "\n\n", postLabel: "b3", label: 3, styles: [5, 7, 8, 9, 16, 19] },
+      {
+        text: "상쾌하고\n가벼운\n입안 감촉",
+        postLabel: "b4",
+        label: 4,
+        styles: [3, 17],
+      },
+    ],
+  },
+  {
+    unique_id: 2,
+    question: "와인을 마실 때, 생각하는 가격대는?",
+    answers: [
+      { text: "5만원 이하", postLabel: "c1", label: 1, styles: [2, 3, 16, 17] },
+      {
+        text: "5만원 ~ 15만원",
+        postLabel: "c2",
+        label: 2,
+        styles: [9, 20, 21, 22],
+      },
+      {
+        text: "15만원 ~ 30만원",
+        postLabel: "c3",
+        label: 3,
+        styles: [4, 5, 8, 10, 11, 12],
+      },
+      {
+        text: "30만원 이상",
+        postLabel: "c4",
+        label: 4,
+        styles: [0, 1, 6, 7, 13, 14, 15, 18],
+      },
+    ],
+  },
+  {
+    unique_id: 3,
+    question: "와인을 마실 때, 선호하는 맛은?",
+    answers: [
+      {
+        text: "묵직하고 힘이 느껴지는 맛 ",
+        postLabel: "d1",
+        label: 1,
+        styles: [0, 13, 15, 18, 20, 21, 22],
+      },
+      {
+        text: "상쾌하고 활기찬 맛",
+        postLabel: "d2",
+        label: 2,
+        styles: [2, 3, 5, 8, 9, 16, 17],
+      },
+      {
+        text: "세련되고 우아한 맛",
+        postLabel: "d3",
+        label: 3,
+        styles: [1, 6, 7, 19],
+      },
+      {
+        text: "다채롭고 섬세한 맛",
+        postLabel: "d4",
+        label: 4,
+        styles: [4, 10, 11, 12, 14],
+      },
+    ],
+  },
+  {
+    unique_id: 4,
+    question: "와인을 마실 때, 어울릴 것 같은 음식은?",
+    answers: [
+      {
+        text: "해산물",
+        postLabel: "e1",
+        label: 1,
+        styles: [4, 5, 6, 7, 8, 9],
+      },
+      {
+        text: "고기",
+        postLabel: "e2",
+        label: 2,
+        styles: [10, 11, 12, 15, 20, 21, 22],
+      },
+      {
+        text: "다양한 음식이 포함된 파인 다이닝",
+        postLabel: "e3",
+        label: 3,
+        styles: [13, 14, 16, 17, 18, 19],
+      },
+      { text: "디저트 치즈", postLabel: "e4", label: 4, styles: [0, 1, 2, 3] },
+    ],
+  },
+  {
+    unique_id: 5,
+    question: "당신이 선호하는 선물 스타일은?",
+    answers: [
+      { text: "감동적인 선물", label: 1, styles: [1, 6, 8, 15, 18, 22] },
+      {
+        text: "유용한 선물",
+        label: 2,
+        styles: [2, 4, 5, 9, 10, 11, 12, 16, 17, 21],
+      },
+      { text: "독특한 선물", label: 3, styles: [0, 3] },
+      { text: "세련된 선물", label: 4, styles: [7, 13, 14, 19, 20] },
+    ],
+  },
+  {
+    unique_id: 6,
+    question:
+      "오랜만에 친구들과 모이는 날이 왔습니다.\n 어떤 모임을 원하시나요?",
+    answers: [
+      {
+        text: "가볍게 만나서 화목하게 떠들면서 편안하게 보내고 싶어요",
+        label: 1,
+        styles: [4, 5, 9, 16, 17],
+      },
+      {
+        text: "친구들과 함께 활동적인 액티비티를 즐기고 싶어요",
+        label: 2,
+        styles: [11, 20, 21, 22],
+      },
+      {
+        text: "조용한 레스토랑에서 오붓하게 시간을 보내고 싶어요",
+        label: 3,
+        styles: [6, 7, 10, 12, 13, 14, 15, 18, 19],
+      },
+      {
+        text: "신나는 파티를 열어서 같이 즐기고 싶어요",
+        label: 4,
+        styles: [0, 1, 2, 3, 8],
+      },
+    ],
+  },
+  {
+    unique_id: 7,
+    question: "여행 중에 어떤 경험을 즐기고 싶으신가요?",
+    answers: [
+      {
+        text: "숨겨진 보석같은 장소를 찾아서 현지인과 함께 놀며 즐기고 싶어요",
+        label: 1,
+        styles: [4, 8, 9, 10, 16, 17],
+      },
+      {
+        text: "아름다운 풍경을 감상하며 고기를 구우면서 캠핑을 하고 싶어요",
+        label: 2,
+        styles: [11, 20, 21, 22],
+      },
+      {
+        text: "멋진 드레스와 수트를 입고 파티에 참여하고 싶어요",
+        label: 3,
+        styles: [0, 1, 2, 3, 5],
+      },
+      {
+        text: "역사와 전통이 묻어나는 곳을 방문하여 오랜 세월의 흔적을 느끼고 싶어요",
+        label: 4,
+        styles: [6, 7, 12, 13, 14, 15, 18, 19],
+      },
+    ],
+  },
+  {
+    unique_id: 8,
+    question: "휴일이나 주말, 나의 취향은?",
+    answers: [
+      {
+        text: "아늑한 공간에서 혼자 책을 읽거나 음악을 듣는다",
+        label: 1,
+        styles: [6, 7, 10, 12, 14, 18, 19],
+      },
+      {
+        text: "친구들과 함께 액티비티를 즐긴다",
+        label: 2,
+        styles: [0, 2, 3, 9, 11, 16, 17, 20, 21],
+      },
+      { text: "유명한 맛집을 찾아다닌다", label: 3, styles: [1, 4, 5, 8] },
+      {
+        text: "전시회나 문화적인 장소에 참여한다",
+        label: 4,
+        styles: [13, 15, 22],
+      },
+    ],
+  },
+  {
+    unique_id: 9,
+    question: "좋아하는 옷 스타일은?",
+    answers: [
+      {
+        text: "클래식하고 우아한 스타일",
+        label: 1,
+        styles: [1, 6, 13, 14, 15, 18, 19],
+      },
+      { text: "밝고 화사한 스타일", label: 2, styles: [2, 3, 7, 9, 16, 17] },
+      { text: "남성적이고 마초적인 스타일", label: 3, styles: [20, 21, 22] },
+      {
+        text: "트렌디하고 개성있는 스타일",
+        label: 4,
+        styles: [0, 4, 5, 8, 10, 11, 12],
+      },
+    ],
+  },
+];
 
 export default questions;
