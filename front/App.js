cf6806c8c50b7c4d37e1ffdaff15b66e53a425da
yewonhihi <yewon1118@naver.com>
2023-07-28 11:08:36 +0900

    [Feat][yewon] add splash screen option
    Signed-off-by: yewonhihi <yewon1118@naver.com>

--- front/App.js
+++ front/App.js
@@ -1,7 +1,28 @@
-import AppNavigation from './StackNavigator';
+import AppNavigation from "./StackNavigator";
+import { ImageBackground, Image } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  return (
-    <AppNavigation />
-  );
+  const [isLoading, setIsLoading] = useState(false); //true로 바꾸면 splash 가능
+  useEffect(() => {
+    // 1,000ms가 1초
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  const Loading = () => {
+    return (
+      <Image
+        source={require("./assets/wineSplash2.jpg")}
+        style={{ width: "100%", height: "100%" }}
+      ></Image>
+    );
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return <AppNavigation />;
+  }
 }
